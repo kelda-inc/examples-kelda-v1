78f0c18ad119132be15be27e6c0f95ded9d6cb59
Kevin Lin <kevin@kelda.io>
2019-10-08 14:48:46 -0700

    Use absolute path to the workspace path
    We were using a relative path before, which meant that the `kelda`
    commands had to be run from a specific directory.

--- golang-sock-shop/configure-kelda.sh
+++ golang-sock-shop/configure-kelda.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-parent_path="$(dirname "${BASH_SOURCE[0]}")"
+parent_path="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 workspace="${parent_path}/kelda-config/workspace.yaml"
 
 if [ "${USING_DEMO_KELDA_CLUSTER}" == "true" ]; then
