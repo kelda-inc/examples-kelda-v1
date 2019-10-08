a41b31bffa859de686841c433c86f016c4996a6a
Kevin Lin <kevin@kelda.io>
2019-10-08 11:48:11 -0700

    Don't prompt during `kelda config` if not necessary
    We now always set the workspace path without prompting, and default to
    the `kelda-demo` context if the user has sourced the
    `use-demo-cluster.sh` script.

--- golang-sock-shop/configure-kelda.sh
+++ golang-sock-shop/configure-kelda.sh
@@ -1,6 +1,11 @@
-#!/bin/sh
+#!/bin/bash
 set -e
 
 parent_path="$(dirname "${BASH_SOURCE[0]}")"
-cd "${parent_path}/kelda-config"
-kelda config
+workspace="${parent_path}/kelda-config/workspace.yaml"
+
+if [ "${USING_DEMO_KELDA_CLUSTER}" == "true" ]; then
+    kelda config --workspace "${workspace}" --context kelda-demo
+else
+    kelda config --workspace "${workspace}"
+fi
--- use-demo-cluster.sh
+++ use-demo-cluster.sh
@@ -5,5 +5,6 @@ kubeconfig_path="${root_path}/demo-kubeconfig"
 
 export PS1="[kelda-demo] $PS1"
 export KUBECONFIG="${kubeconfig_path}"
+export USING_DEMO_KELDA_CLUSTER="true"
 
 echo "You are now connected to the Kelda demo cluster."
