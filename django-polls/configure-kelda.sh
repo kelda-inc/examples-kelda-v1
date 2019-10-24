5f372bc15f04e77c84610a1db4c10298c16332c6
Kevin Lin <kevin@kelda.io>
2019-10-24 13:49:39 -0700

    polls: Add script for setting up ~/.kelda.yaml

---
+++ django-polls/configure-kelda.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -e
+
+parent_path="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+workspace="${parent_path}/kelda-workspace/workspace.yaml"
+
+if [ "${USING_DEMO_KELDA_CLUSTER}" == "true" ]; then
+    kelda config --workspace "${workspace}" --context kelda-demo
+else
+    kelda config --workspace "${workspace}"
+fi
