250bfafd90fc9fe86aefa59dab3da6f51fa4c4f4
Kevin Lin <kevin@kelda.io>
2019-11-15 15:03:25 -0800

    django-polls: Boot cleanly in no-sync mode

--- django-polls/src/mysite/settings.py
+++ django-polls/src/mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'your_secret_key_here'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
