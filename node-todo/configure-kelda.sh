dd00cdf9b924215b56d240af3a77ea66550bc0f6
Kevin Lin <kevin@kelda.io>
2019-11-28 10:33:50 -0800

    Use demo cluster by setting magic context
    Rather than source-ing the `use-demo-cluster.sh` script, Kelda now
    supports connecting to the demo cluster by setting the Kelda context to
    kelda-demo-cluster.

--- node-todo/configure-kelda.sh
+++ node-todo/configure-kelda.sh
@@ -4,8 +4,8 @@ set -e
 parent_path="$(command cd "$(dirname "${BASH_SOURCE[0]}")" && command pwd)"
 workspace="${parent_path}/kelda-workspace/workspace.yaml"
 
-if [ "${USING_DEMO_KELDA_CLUSTER}" == "true" ]; then
-    kelda config --workspace "${workspace}" --context kelda-demo
+if [ "${1}" == "--use-demo-cluster" ]; then
+    kelda config --workspace "${workspace}" --context kelda-demo-cluster
 else
     kelda config --workspace "${workspace}"
 fi
